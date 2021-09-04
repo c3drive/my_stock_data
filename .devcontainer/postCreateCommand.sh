81f97d1e8ca9c23bd5377fbdce0be7f6d7a9421d
c3drive <mihara000satsuki@yahoo.co.jp>
2021-09-04 23:18:49 +0900

    first commit

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Installing Developer Requirements"
+
+# AWS KEY
+cp -rf /workspace/.aws ~/
+
+# Docker Install
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+#apt-cache madison docker-ce
+apt-get install -y docker-ce=5:20.10.8~3-0~debian-bullseye docker-ce-cli=5:20.10.8~3-0~debian-bullseye containerd.io
+
+# AWS SAM CLI Install
+wget  -P /tmp https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
+unzip /tmp/aws-sam-cli-linux-x86_64.zip -d /tmp/sam-installation
+/tmp/sam-installation/install
+rm -rf /tmp/aws-sam-cli-linux-x86_64.zip
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# my_stock_data
+## This repo
+```bash
+PC(VS Code, Docker for Container1)
+└── Container1(Docker for Container2, AWS SAM, Python etc..)
+    └── Container2(Run functions locally and invoke)
+```
+## Common in Container1
+```bash
+$ service docker start
+```
+
+## lambda-python3.9 in Container1
+Run functions locally and invoke them
+```bash
+$ cd lambda-python3.9
+$ sam build
+$ sam local invoke
+```
+
+Use the `sam local start-api` to run the API locally on port 3000.
+```bash
+$ cd lambda-python3.9
+$ sam build
+$ sam local start-api
+```
+```
+$ curl http://127.0.0.1:3000/hello
+{"message": "hello world"}
+```
+To build and deploy
+```bash
+$ cd lambda-python3.9
+$ sam build
+$ sam deploy --guided
+```
+General information about this SAM project can be found in the [`README.md`](./lambda-python3.9/README.md) file in lambda-python3.9 folder.
